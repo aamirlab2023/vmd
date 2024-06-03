dad742800b0c28318bbb71a33533088b6bcce035
aamirlab2023 <136485053+aamirlab2023@users.noreply.github.com>
2024-06-03 15:51:11 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/winsomes/tools
-tar -zxvf vmd-1.9.4a55.bin.LINUXAMD64-CUDA102-OptiX650-OSPRay185-RTXRTRT.opengl.tar.gz -C ~/winsomes/tools/
+tar -zxvf vmd-1.9.2*.tar.gz -C ~/winsomes/tools/
 mv ~/winsomes/tools/vmd-* ~/winsomes/tools/vmd
 cd ~/winsomes/tools/vmd && ./configure
 cd ~/winsomes/tools/vmd/src && sudo make install
